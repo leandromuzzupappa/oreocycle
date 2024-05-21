e12386c4dd011cfd12048ec22be519626d12c3cf
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-05-21 02:33:09 -0300

    update homepage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { O1Header } from "@organisms/O1Header";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Pepitos</h1>
-    </main>
+    <>
+      <O1Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Pepitos</h1>
+      </main>
+    </>
   );
 }
