dbe8c16547bbb3d7a25a7302c98ad8a7c3c0ffc0
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-05-21 23:57:32 -0300

    update o2 component with style classes

--- src/components/organisms/O2ArticleCards.tsx
+++ src/components/organisms/O2ArticleCards.tsx
@@ -1,6 +1,6 @@
 import { M2Card, M2CardProps } from "@molecules/M2Card";
 
-type O2ArticleCardsProps = {
+export type O2ArticleCardsProps = {
   headline: string;
   description: string;
   cards: M2CardProps[];
@@ -13,16 +13,22 @@ export const O2ArticleCards = ({
 }: O2ArticleCardsProps) => {
   return (
     <section className="">
-      <h2>{headline}</h2>
-      <p>{description}</p>
+      <h2 className="mb-4 text-3xl font-bold text-black">{headline}</h2>
+      <p className="mb-11 text-base font-medium text-secondary-lightest">
+        {description}
+      </p>
 
-      <ul>
-        {cards.map((card, index) => (
-          <li key={index}>
-            <M2Card {...card} />
-          </li>
-        ))}
-      </ul>
+      {cards.length >= 0 ? (
+        <ul className="grid w-full gap-7 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {cards.map((card) => (
+            <li key={`o2-item-${card.slug}`}>
+              <M2Card {...card} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No products found</p>
+      )}
     </section>
   );
 };
