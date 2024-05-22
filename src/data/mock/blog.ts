2e6723dc5bb533ac56a3d3699b88da473e58c390
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-05-22 17:32:36 -0300

    add local images and update blog mock images path

--- src/data/mock/blog.ts
+++ src/data/mock/blog.ts
@@ -4,62 +4,53 @@ export const blogMock: Blog[] = [
   {
     headline:
       "The Meteor 350 nails the classic cruiser vibe with its design, looks, and relaxed ergonomics, offering an easygoing ride with predictable power and handling.",
-    image:
-      "https://www.royalenfield.com/content/dam/royal-enfield/usa/motorcycles/meteor/media-review/cycle-world.jpg",
+    image: "/images/cycle-world.jpg",
     url: "https://www.cycleworld.com/story/motorcycle-reviews/2021-royal-enfield-meteor-350-first-ride-review/",
   },
   {
     headline:
       "Royal Enfield’s Meteor 350 has the kind of welcoming, carefree attitude that is sure to leave an impression, thanks to its gentle handling, approachable power and good looks.",
-    image:
-      "https://www.royalenfield.com/content/dam/royal-enfield/usa/motorcycles/meteor/media-review/rider-magazine-2.png",
+    image: "/images/rider-magazine-2.png",
     url: "https://ridermagazine.com/2021/04/07/2021-royal-enfield-meteor-350-road-test-review/",
   },
   {
     headline:
       "The Meteor is a fantastic little motorcycle. It doesn't look or feel like a cheap bike and any new rider that throws a leg over one will love it.",
-    image:
-      "https://www.royalenfield.com/content/dam/royal-enfield/usa/motorcycles/meteor/media-review/c-net.jpg",
+    image: "/images/c-net.jpg",
     url: "https://www.cnet.com/roadshow/news/2021-royal-enfield-meteor-350-review/",
   },
   {
     headline:
       "The Meteor 350 is all new from the wheels up, and is a clever mix of high tech, retro style, and comfort.",
-    image:
-      "https://www.royalenfield.com/content/dam/royal-enfield/usa/motorcycles/meteor/media-review/forbes.png",
+    image: "/images/forbes.png",
     url: "https://www.forbes.com/sites/billroberson/2021/04/06/royal-enfield-serves-up-a-retro-cool-beginner-bike-with-the-new-meteor-350/?sh=7f03798811ed",
   },
   {
     headline:
       "Meteor Streaks Into U.S. Market — Royal Enfield's Middleweight Meteor 350",
-    image:
-      "https://www.royalenfield.com/content/dam/royal-enfield/usa/motorcycles/meteor/media-review/issuu.jpg",
+    image: "/images/issuu.jpg",
     url: "https://issuu.com/dealernews/docs/issue_5_may_low_res/24",
   },
   {
     headline: "Five Reasons to Buy a Motorcycle This Year",
-    image:
-      "https://www.royalenfield.com/content/dam/royal-enfield/usa/motorcycles/meteor/media-review/five-reason.jpg",
+    image: "/images/five-reason.jpg",
     url: "https://www.gearpatrol.com/cars/a36179355/five-reasons-to-buy-a-motorcycle-this-year/",
   },
   {
     headline: "Is This All-New Motorcycle Greater Than the Sum of its Parts?",
-    image:
-      "https://www.royalenfield.com/content/dam/royal-enfield/usa/motorcycles/meteor/media-review/all-new-motorcycle.jpg",
+    image: "/images/all-new-motorcycle.jpg",
     url: "https://www.royalenfield.com/us/en/our-world/media/news/product-reviews/is-this-all-new-motorcycle",
   },
   {
     headline:
       "With intuitive handling, standard ABS, a fantastic motor, room to spread out, and nicely finished styling, the Meteor 350 is a great motorcycle.",
-    image:
-      "https://www.royalenfield.com/content/dam/royal-enfield/usa/motorcycles/meteor/media-review/ultimate-motorcycle.jpg",
+    image: "/images/ultimate-motorcycle.jpg",
     url: "https://ultimatemotorcycling.com/2021/04/09/new-riders-buyers-guide-to-motorcycles-best-cruisers-of-2021/",
   },
   {
     headline:
       "The Meteor 350 nails the classic cruiser vibe with its design, looks, and relaxed ergonomics, offering an easygoing ride with predictable power and handling.",
-    image:
-      "https://www.royalenfield.com/content/dam/royal-enfield/usa/motorcycles/meteor/media-review/cycle-world.jpg",
+    image: "/images/cycle-world.jpg",
     url: "https://www.cycleworld.com/story/motorcycle-reviews/2021-royal-enfield-meteor-350-first-ride-review/",
   },
 ];
