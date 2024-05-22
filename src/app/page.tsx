1f6052a1b724155c4c97d825fd483737d8533c43
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-05-21 23:58:46 -0300

    update homepage with t1 and t2 component templates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { O1Header } from "@organisms/O1Header";
+import { T1HomeAccessories } from "@templates/T1HomeAccessories";
+import { T2HomeBlog } from "@templates/T2HomeBlog";
 
 export default function Home() {
   return (
     <>
       <O1Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Pepitos</h1>
+      <main className="mx-auto w-full max-w-[1110px] px-4 lg:px-0">
+        <T1HomeAccessories />
+        <T2HomeBlog />
       </main>
     </>
   );
