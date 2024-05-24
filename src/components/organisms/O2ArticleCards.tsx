7bb68d8b008fb81e7042a25c28f4608cc86db477
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-05-24 10:12:55 -0300

    update title and description with the m6 component

--- src/components/organisms/O2ArticleCards.tsx
+++ src/components/organisms/O2ArticleCards.tsx
@@ -1,4 +1,5 @@
 import { M2Card, M2CardProps } from "@molecules/M2Card";
+import { M6TextContent } from "@molecules/M6TextContent";
 
 export type O2ArticleCardsProps = {
   headline: string;
@@ -13,12 +14,9 @@ export const O2ArticleCards = ({
 }: O2ArticleCardsProps) => {
   return (
     <section className="">
-      <h2 className="mb-4 text-3xl font-bold text-black">{headline}</h2>
-      <p className="mb-11 text-base font-medium text-secondary-lightest">
-        {description}
-      </p>
+      <M6TextContent headline={headline} description={description} />
 
-      {cards.length >= 0 ? (
+      {cards.length >= 0 && (
         <ul className="grid w-full gap-7 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {cards.map((card) => (
             <li key={`o2-item-${card.slug}`}>
@@ -26,8 +24,6 @@ export const O2ArticleCards = ({
             </li>
           ))}
         </ul>
-      ) : (
-        <p>No products found</p>
       )}
     </section>
   );
