1d5067dcb9c013463177e64a6e985f2bc64cb5b6
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-05-24 10:14:43 -0300

    update t4 with products data

--- src/components/templates/T4HomeFeatureProducts.tsx
+++ src/components/templates/T4HomeFeatureProducts.tsx
@@ -1,17 +1,45 @@
 "use client";
-/* import { useEffect, useState } from "react";
-import { A4SliderSlide } from "@atoms/A4SliderSlide"; */
-import { M5ProductCard } from "@molecules/M5ProductCard";
-/* import { O3Slider, O3OptionsType } from "@organisms/O3Slider";
+import { useEffect, useState } from "react";
+import { A4SliderSlide } from "@atoms/A4SliderSlide";
+import { M5ProductCard, M5ProductCardCTAProps } from "@molecules/M5ProductCard";
+import { M6TextContent } from "@molecules/M6TextContent";
+import { O3Slider, O3OptionsType } from "@organisms/O3Slider";
 import { getProducts } from "@services/get-products";
-import { CategoryEnum } from "@data/types/product"; */
+import { CategoryEnum } from "@data/types/product";
 
 export const T4HomeFeatureProducts = () => {
-  /* const [sliderData, setSliderData] = useState([]);
+  const [sliderData, setSliderData] = useState<M5ProductCardCTAProps[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const products = await getProducts(CategoryEnum.MOTOCYCLES);
+      const banProducts = [
+        "Interceptor 650 Standard",
+        "Continental GT 650 Standard",
+      ];
+      const data = products.filter(
+        ({ name, seller }) =>
+          seller.name === "Lema Motors" && !banProducts.includes(name),
+      );
+
+      const sliderData: M5ProductCardCTAProps[] = data.map(
+        ({ name, variants, slug }) => ({
+          variant: "CTA",
+          name,
+          image: variants[0].images[0].formats?.thumbnail?.url || "",
+          link: `/${slug}`,
+        }),
+      );
+
+      setSliderData(sliderData);
+    };
+
+    fetchData();
+  }, []);
 
   const options: O3OptionsType = {
     classList: {
-      container: "border w-full",
+      container: "w-full",
       slide: "h-[450px]",
     },
     arrows: true,
@@ -19,33 +47,23 @@ export const T4HomeFeatureProducts = () => {
     loop: true,
     slidesPerView: 4,
     align: "start",
-  }; */
+  };
 
   return (
-    <section className="w-full py-8">
-      {/* <O3Slider options={options}>
-        <A4SliderSlide>
-          <M5ProductCard />
-        </A4SliderSlide>
-        <A4SliderSlide>
-          <M5ProductCard />
-        </A4SliderSlide>
-        <A4SliderSlide>
-          <M5ProductCard />
-        </A4SliderSlide>
-        <A4SliderSlide>
-          <M5ProductCard />
-        </A4SliderSlide>
-        <A4SliderSlide>
-          <M5ProductCard />
-        </A4SliderSlide>
-      </O3Slider> */}
-
-      <M5ProductCard
-        variant="full"
-        name="Pepitos"
-        image="https://bucket-rn-40-dev-test.s3.amazonaws.com/thumbnail_large_c94a96bb_0597_4243_a2cc_e73293c98e8b_e91e3683a9.webp"
+    <section className="w-full py-16 text-center">
+      <M6TextContent
+        headline="Conoce a la Familia Meteor"
+        description="Sapien, duis commodo risus molestie pulvinar sit id id. Malesuada quam leo, quis venenatis integer pellentesque mauris."
+        align="center"
       />
+
+      <O3Slider options={options}>
+        {sliderData.map((props, index) => (
+          <A4SliderSlide key={index}>
+            <M5ProductCard {...props} classList="border-0" />
+          </A4SliderSlide>
+        ))}
+      </O3Slider>
     </section>
   );
 };
