bcd50d94d8ea960991ef86ba6609ed1b7879ecec
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-05-24 10:09:16 -0300

    create mock data for motocycle features and swatches

---
+++ src/data/mock/motocycleFeatures.ts
@@ -0,0 +1,27 @@
+import { FeatureType } from "@molecules/M5ProductCard";
+
+export const mockFeaturesDateTime: FeatureType[] = [
+  {
+    type: "date",
+    value: "15 de Marzo",
+  },
+  {
+    type: "time",
+    value: "2 Horas",
+  },
+];
+
+export const mockFeaturesProductFeatures: FeatureType[] = [
+  {
+    type: "engine",
+    value: "500cc",
+  },
+  {
+    type: "category",
+    value: "Classic",
+  },
+  {
+    type: "height",
+    value: "1.2m",
+  },
+];
---
+++ src/data/mock/motocycleSwatches.ts
@@ -0,0 +1,31 @@
+import { SwatchesType } from "@molecules/M5ProductCard";
+
+export const swatchesMock: SwatchesType[] = [
+  {
+    color: "#FF0000",
+    image:
+      "https://bucket-rn-40-dev-test.s3.amazonaws.com/thumbnail_large_c94a96bb_0597_4243_a2cc_e73293c98e8b_e91e3683a9.webp",
+    label: "Fireball Red",
+  },
+  {
+    color: "#0000FF",
+    image:
+      "https://bucket-rn-40-dev-test.s3.amazonaws.com/thumbnail_large_c94a96bb_0597_4243_a2cc_e73293c98e8b_e91e3683a9.webp",
+    label: "Ocean Blue",
+  },
+  {
+    color: "#000000",
+    image:
+      "https://bucket-rn-40-dev-test.s3.amazonaws.com/thumbnail_large_c94a96bb_0597_4243_a2cc_e73293c98e8b_e91e3683a9.webp",
+    label: "Coal Black",
+  },
+];
+
+export const swatchesMockWithOne: SwatchesType[] = [
+  {
+    color: "#FF0000",
+    image:
+      "https://bucket-rn-40-dev-test.s3.amazonaws.com/thumbnail_large_c94a96bb_0597_4243_a2cc_e73293c98e8b_e91e3683a9.webp",
+    label: "Fireball Red",
+  },
+];
