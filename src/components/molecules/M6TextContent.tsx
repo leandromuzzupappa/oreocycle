734d813eb8ea65690285ee7499fbd1a92adf3b71
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-05-24 10:11:20 -0300

    create m6 text content

---
+++ src/components/molecules/M6TextContent.tsx
@@ -0,0 +1,41 @@
+type M6TextContentProps = {
+  headline: string;
+  description?: string;
+  eyebrow?: string;
+  align?: "left" | "center" | "right";
+};
+
+export const M6TextContent = ({
+  headline,
+  description,
+  eyebrow,
+  align = "left",
+}: M6TextContentProps) => {
+  const alignClass = {
+    left: "text-left",
+    center: "text-center",
+    right: "text-right",
+  }[align];
+
+  return (
+    <>
+      {eyebrow && (
+        <p
+          className={`text-sm font-semibold uppercase text-purple ${alignClass}`}
+        >
+          {eyebrow}
+        </p>
+      )}
+      <h2 className={`mb-4 text-3xl font-bold text-black ${alignClass}`}>
+        {headline}
+      </h2>
+      {description && (
+        <p
+          className={`mb-11 w-full text-pretty text-base font-medium text-secondary-lightest ${align === "center" ? `${alignClass} mx-auto lg:w-[530px]` : `${alignClass}`} `}
+        >
+          {description}
+        </p>
+      )}
+    </>
+  );
+};
