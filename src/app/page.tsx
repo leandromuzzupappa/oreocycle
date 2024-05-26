a0138f064a661ef52ce3ec5061aa695de49e597c
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-05-26 03:09:05 -0300

    update homepage with m7 component and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { O1Header } from "@organisms/O1Header";
+import { O4Footer } from "@organisms/O4Footer";
+import { M7TextBanner } from "@molecules/M7TextBanner";
 import { T1HomeAccessories } from "@templates/T1HomeAccessories";
 import { T2HomeBlog } from "@templates/T2HomeBlog";
 import { T3HomeHeroSlider } from "@templates/T3HomeHeroSlider";
@@ -9,13 +11,19 @@ export default function Home() {
   return (
     <>
       <O1Header />
-      <main className="mx-auto w-full max-w-[1110px] px-4 lg:px-0">
+      <main className="mx-auto w-full max-w-[1110px] px-4 pt-[75px] lg:px-0">
         <T3HomeHeroSlider />
         <T4HomeFeatureProducts />
+        <M7TextBanner
+          headline="Entérate del último recorrido de la Meteor 350 por las sierras de Córdoba."
+          linkLabel="Leer nota completa"
+          link="#newsletter"
+        />
         <T1HomeAccessories />
         <T5HomeEvents />
         <T2HomeBlog />
       </main>
+      <O4Footer />
     </>
   );
 }
